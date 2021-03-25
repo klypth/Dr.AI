b2e7ddcac288cf90338161b15fde05c1453f3835
nicolas_lu <76612512+klypth@users.noreply.github.com>
2021-03-25 18:24:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        text=event.message.text
+        TextSendMessage(text='halolo'))
 
 
 if __name__ == "__main__":
